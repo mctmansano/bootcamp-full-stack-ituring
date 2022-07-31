3d893fe17b4c542c9021294a9796352df250fc0b
Maria Carolina Mansano <mctmansano@gmail.com>
2022-07-31 12:38:33 -0300

    Adicionar arquivo Etapa 6 - prática 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+let nome = "Maria Carolina"
+let email = "mctmansano@gmail.com"
+let profissão = "Analista de Qualidade"
+let idade = 28
+let lugardosonho = "Europa"
+
+console.log (typeof nome == "string")
+console.log (typeof email == "string")
+console.log (typeof profissão == "string")
+console.log (typeof idade == "string")
+console.log (typeof lugardosonho == "string")
