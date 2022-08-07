cab01d51cd3fba98c6f2dca9a388fbb13df3a4ab
Maria Carolina Mansano <mctmansano@gmail.com>
2022-08-07 20:06:23 -0300

    Adicionar arquivo Etapa 3 - prática 7 e 8

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,13 @@
+function idiomaEscolhido(idioma) {
+if (idioma === "es") {
+    console.log("¡Hola mundo!")
+} else if (idioma === "pt") {
+    console.log("Olá, Mundo!")
+} else if (idioma === "en") {
+    console.log("Hello, world!")
+} else {
+    console.log("Olá, Mundo!")
+}
+}
+
+idiomaEscolhido()
---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -0,0 +1,9 @@
+function numeroParOuImpar (numero) {
+    if (numero%2 === 0) {
+        console.log (`O número ${numero} é par`)
+    } else {
+        console.log (`O número ${numero} é ímpar`)
+    }
+}
+
+numeroParOuImpar(7)
