608d9128e104194b47828d6377cd3260ef066478
Maria Carolina Mansano <mctmansano@gmail.com>
2022-08-07 20:38:52 -0300

    Adicionar arquivo Etapa 3 - prática 10

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -0,0 +1,20 @@
+function esseEhOMaiorNumero (numero1, numero2, numero3) {
+if (numero1>numero2 && numero1>numero3)
+console.log(numero1)
+    else if (numero2>numero1 && numero2>numero3)
+console.log(numero2)
+    else if (numero3>numero1 && numero3>numero2) 
+console.log(numero3)
+else
+console.log (numero2)
+}
+
+esseEhOMaiorNumero(1,2,3)
+
+esseEhOMaiorNumero(6,5,4)
+
+esseEhOMaiorNumero(7,7,7)
+
+esseEhOMaiorNumero(6,15,4)
+
+esseEhOMaiorNumero(32,20,45)
