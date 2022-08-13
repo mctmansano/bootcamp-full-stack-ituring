2607f89a94584a566cd6cb8b9cfa46fdb8724164
Maria Carolina Mansano <mctmansano@gmail.com>
2022-08-12 22:49:39 -0300

    Adicionar arquivo Tarefa 3 - prática 1

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
@@ -0,0 +1,12 @@
+
+    let i = 1
+    let multiplicador = 9
+
+function tabuada () {
+    
+    while (i <= 10) {
+        console.log (i + " X " + multiplicador + " = " + (i * multiplicador))
+        i += 1
+    }
+}
+tabuada()
