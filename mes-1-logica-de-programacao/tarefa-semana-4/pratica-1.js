496413ddbb34fd14ff5fc023c8a9325e1de1184c
Maria Carolina Mansano <mctmansano@gmail.com>
2022-08-21 10:45:12 -0300

    Adicionar arquivos Tarefa 4 - prática-1

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-1.js
@@ -0,0 +1,10 @@
+let listaDeSeries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+
+function itemESeries(nomedeSeries, indice){
+    console.log([indice], nomedeSeries)
+}
+
+listaDeSeries.forEach(itemESeries)
+
+
+
