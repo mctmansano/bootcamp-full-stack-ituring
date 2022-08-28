1b7991e2a102525c4865e2bded4ff0eae42d5a04
Maria Carolina Mansano <mctmansano@gmail.com>
2022-08-28 19:57:18 -0300

    Adicionar arquivo Tarefa 4 - prática 14

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
@@ -2,32 +2,39 @@ let caracteristicaDosResturantes = {
     restaurante: "Turing Gourmet",
     tipo: "Comida britânica",
     avaliações: [
-      {
-        cliente: "Thais S.",
-        nota: 9.8
-      },
-      {
-        cliente: "Thales Gonçalves",
-        nota: 8.9
-      },
-      {
-        cliente: "José Lopes",
-        nota: 9.9
-      },
-      {
-        cliente: "Cristina Souza",
-        nota: 9.3
-      },
-      {
-        cliente: "Leo Garcia",
-        nota: 8.5
-      }
+        {
+            cliente: "Thais S.",
+            nota: 9.8
+        },
+        {
+            cliente: "Thales Gonçalves",
+            nota: 8.9
+        },
+        {
+            cliente: "José Lopes",
+            nota: 9.9
+        },
+        {
+            cliente: "Cristina Souza",
+            nota: 9.3
+        },
+        {
+            cliente: "Leo Garcia",
+            nota: 8.5
+        }
     ]
-  }
+}
 
-function mediaDasAvaliacoes (notasDasPessoas) {
-    let notasDasAvaliacoes = notasDasPessoas.map((notas) => caracteristicaDosResturantes.avaliações)
-    console.log(notasDasAvaliacoes)
-  }
+function mediaDasAvaliacoes(avaliacoes) {
+    let notas = avaliacoes.avaliações.map(function (avaliações) {
+        return avaliações.nota
+    })
+    let somaDasNotas = notas.reduce(function (soma, i) {
+        return soma + i
+    })
+    let media = somaDasNotas / notas.length
+
+    console.log(`A média de avaliações do restaurante Turing Gourmet foi de ${media.toFixed(2)}.`)
+}
 
 mediaDasAvaliacoes(caracteristicaDosResturantes)
