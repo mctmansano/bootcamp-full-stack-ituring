f4021f0859be651c43d4b98d807506974460c236
Maria Carolina Mansano <mctmansano@gmail.com>
2022-09-05 05:58:04 -0300

    Corrigir arquivos prática 3 e 6

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-6.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-6.js
@@ -1,12 +1,3 @@
 let valorProdutoComDesconto = (valorProduto, porcentagemDeDesconto) => valorProduto * (1 - (porcentagemDeDesconto/100))
 
 console.log (valorProdutoComDesconto(100, 5))
-
-
-
-/* let valorProdutoEmPromoçao = (valorProduto, porcentagemDeDesconto) => {
-
-    return (valorProduto * (1 - (porcentagemDeDesconto/100)))
-}
-
-console.log (valorProdutoEmPromoçao(100, 20))*/
