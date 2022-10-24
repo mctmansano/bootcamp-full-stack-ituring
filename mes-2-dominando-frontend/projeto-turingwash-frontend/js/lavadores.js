8d9f43656e1ba566d54b032cf2d3c74dab6d7a1e
Maria Carolina Mansano <mctmansano@gmail.com>
2022-10-24 18:44:52 -0300

    Adicionar etapa 2 da semana 4

---
+++ mes-2-dominando-frontend/projeto-turingwash-frontend/js/lavadores.js
@@ -0,0 +1,26 @@
+var lavadores = [
+    {
+        "nome": "Matheus Oliveira",
+        "nota": 4.3,
+        "qtdAvaliacoes":  783,
+        "valorCobrado": 90
+    },
+    {
+        "nome": "Pedro Sampaio",
+        "nota": 4.1,
+        "qtdAvaliacoes":  801,
+        "valorCobrado": 60
+    },
+    {
+        "nome": "João Pereira",
+        "nota": 4.5,
+        "qtdAvaliacoes":  998,
+        "valorCobrado": 80
+    },
+    {
+        "nome": "Joana Santos",
+        "nota": 4.7,
+        "qtdAvaliacoes":  489,
+        "valorCobrado": 75
+    },
+]
